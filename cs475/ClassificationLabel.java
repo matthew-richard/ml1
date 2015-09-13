19995470ebcfc18bd67cc00afec08b92c2e3c473
Matt Richard <mricha56@jhu.edu>
2015-09-13 01:03:51 -0400

    Implemented ClassificationLabel and RegressionLabel

--- cs475/ClassificationLabel.java
+++ cs475/ClassificationLabel.java
@@ -4,14 +4,15 @@ import java.io.Serializable;
 
 public class ClassificationLabel extends Label implements Serializable {
 
+	int label;
+
 	public ClassificationLabel(int label) {
-		// TODO Auto-generated constructor stub
+		this.label = label;
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return ((Integer)label).toString();
 	}
 
 }
--- cs475/RegressionLabel.java
+++ cs475/RegressionLabel.java
@@ -4,14 +4,15 @@ import java.io.Serializable;
 
 public class RegressionLabel extends Label implements Serializable {
 
+	double label;
+
 	public RegressionLabel(double label) {
-		// TODO Auto-generated constructor stub
+		this.label = label;
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return ((Double)label).toString();
 	}
 
 }
