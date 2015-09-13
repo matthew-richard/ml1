b739816c966e2ffaddba80f5c5642b145c9e5133
Matt Richard <mricha56@jhu.edu>
2015-09-13 03:38:04 -0400

    Implemented AccuracyEvaluator

--- cs475/AccuracyEvaluator.java
+++ cs475/AccuracyEvaluator.java
@@ -1,15 +1,25 @@
 package cs475;
 
-//TODO: Implement AccuracyEvaluator
-
 import java.util.List;
 
 /**
  * Created by matt on 9/13/15.
  */
 public class AccuracyEvaluator extends Evaluator {
+
     @Override
+    /**
+     * @return Correct classification rate, in [0,1].
+     */
     public double evaluate(List<Instance> instances, Predictor predictor) {
-        return 0;
+        int numCorrect = 0;
+
+        for (Instance instance : instances) {
+            if (predictor.predict(instance).equals(instance.getLabel())) {
+                numCorrect++;
+            }
+        }
+
+        return (double)numCorrect / (double)instances.size();
     }
 }
