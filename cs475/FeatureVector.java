a6acfd91cb66646d6da5e58a14128557e9008932
Matt Richard <mricha56@jhu.edu>
2015-09-13 18:25:04 -0400

    Edited run script, fixed null pointer in FeatureVector

--- cs475/FeatureVector.java
+++ cs475/FeatureVector.java
@@ -1,12 +1,13 @@
 package cs475;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FeatureVector implements Serializable {
 
     // Nonzero vector elements
-	private Map<Integer, Double> supports;
+	private Map<Integer, Double> supports = new HashMap<>();
 
 	public void add(int index, double value) {
         supports.put(index, value);
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 # Run from project root
 
-RUN='java -cp ".:./lib/library.jar:/usr/share/java/commons-cli-1.2.jar"'
+#RUN='java -cp ".:./lib/library.jar:/usr/share/java/commons-cli-1.2.jar"';
+#$RUN cs475.Classify
 
-$RUN cs475.Classify
+java -cp ".:./lib/libarary.jar:/usr/share/java/commons-cli-1.2.jar" $@
