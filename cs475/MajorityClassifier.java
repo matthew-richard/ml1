196e903cdf2237adfbc6e4c1b3128448966d3fd4
Matt Richard <mricha56@jhu.edu>
2015-09-13 03:58:22 -0400

    Implemented MajorityClassifier

--- cs475/MajorityClassifier.java
+++ cs475/MajorityClassifier.java
@@ -1,7 +1,9 @@
 package cs475;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // TODO: Implement MajorityClassifier
 
@@ -9,13 +11,38 @@ import java.util.List;
  * Created by matt on 9/13/15.
  */
 public class MajorityClassifier extends Predictor implements Serializable {
+
+    ClassificationLabel majorityLabel = null;
+
     @Override
     public void train(List<Instance> instances) {
+        Map<ClassificationLabel, Integer> labelCounts = new HashMap<>();
 
+        for (Instance instance : instances) {
+            if (instance.getLabel().equals(new ClassificationLabel(-1)))
+                continue;
+
+            int labelCount = 0;
+            if (labelCounts.containsKey(instance.getLabel()))
+                labelCount = labelCounts.get(instance.getLabel());
+
+            labelCounts.put((ClassificationLabel)instance.getLabel(), ++labelCount);
+        }
+
+        int maxCount = 0;
+        ClassificationLabel majority = null;
+        for (Map.Entry<ClassificationLabel, Integer> count : labelCounts.entrySet()) {
+            if (count.getValue() > maxCount) {
+                maxCount = count.getValue();
+                majority = count.getKey();
+            }
+        }
+
+        majorityLabel = majority;
     }
 
     @Override
     public Label predict(Instance instance) {
-        return null;
+        return majorityLabel;
     }
 }
