37dbf2183f8f974fd0bfd718401eee2757313db6
Matt Richard <mricha56@jhu.edu>
2015-09-13 00:58:10 -0400

    Implemented FeatureVector

--- cs475/FeatureVector.java
+++ cs475/FeatureVector.java
@@ -1,17 +1,22 @@
 package cs475;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public class FeatureVector implements Serializable {
 
+    // Nonzero vector elements
+	private Map<Integer, Double> supports;
+
 	public void add(int index, double value) {
-		// TODO Auto-generated method stub
-		
+        supports.put(index, value);
 	}
 	
 	public double get(int index) {
-		// TODO Auto-generated method stub
-		return 0;
+        Double value = supports.get(index);
+        if (value == null)
+            value = 0.0;
+		return value;
 	}
 
 }
