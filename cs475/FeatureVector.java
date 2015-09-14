e53a1d754a36d16fc62e0422f1c5dd9148262279
Matt Richard <mricha56@jhu.edu>
2015-09-14 11:13:50 -0400

    Added iterator() to FeatureVector

--- cs475/FeatureVector.java
+++ cs475/FeatureVector.java
@@ -1,8 +1,13 @@
 package cs475;
 
+import javafx.util.Pair;
+
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
 
 public class FeatureVector implements Serializable {
 
@@ -18,6 +23,30 @@ public class FeatureVector implements Serializable {
         if (value == null)
             value = 0.0;
 		return value;
+	}
+
+	public Iterator<Pair<Integer, Double>> nonzeroIterator() {
+		return new FeatureVectorIterator(this);
+	}
+
+	private static class FeatureVectorIterator implements Iterator<Pair<Integer, Double>> {
+
+		protected Iterator<Map.Entry<Integer, Double>> setIterator = null;
+
+		public FeatureVectorIterator (FeatureVector vector) {
+			setIterator = vector.supports.entrySet().iterator();
+		}
+
+		@Override
+		public boolean hasNext() {
+			return setIterator.hasNext();
+		}
+
+		@Override
+		public Pair<Integer, Double> next() {
+			Map.Entry<Integer, Double> entry = setIterator.next();
+			return new Pair<Integer,Double> (entry.getKey(), entry.getValue());
+		}
 	}
 
 }
